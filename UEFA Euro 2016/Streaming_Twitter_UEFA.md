dfdf17a7a00c4c567cdaaffc5a9945963be24f83
fzd9752 <fzd975@msn.com>
2016-07-11 01:19:40 +0800

    UEFA Final

---
+++ UEFA Euro 2016/Streaming_Twitter_UEFA.md
@@ -0,0 +1,11 @@
+# How to streaming the tweets to txt with JSON
+
+1. Download the *streaming_json.py* to your local directory.
+2. Open Command-Line tool, then direct the directory to the download file
+3. Run below code in your Command-Line tool
+
+```
+$ python streaming_UEFA_Final_json.py > twitter_UEFA_raw.txt
+```
+
+Now you should have a complicated .txt file concluding 1000 tweets we searched in JSON format.
---
+++ UEFA Euro 2016/streaming_UEFA_Final_json.py
@@ -0,0 +1,42 @@
+# Prepare Environment
+
+## Import JSON library
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+## Import "twitter" library
+from twitter import *
+
+## Set up credentials to access Twitter API
+ACCESS_TOKEN = '85621908-Mi8AHFgdxfgz6crNIpHvomR9qYzIG8u696EQY73Tv'
+ACCESS_SECRET = 'YQ5dR4P2UChL8n7PLbyT2maVUTmZgcrhEi5yJo4xE1JAb'
+CONSUMER_KEY = 'W26R2xhOik4m3tpqqcB1krwjZ'
+CONSUMER_SECRET = 'yaJhOEmVR3VDRwDBVaAaCKgnHDLu6RpxfyM2I5qaj1bZu1Q6ah'
+
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+## Initiate the connection to Twitter Streaming API
+twitter_stream = TwitterStream(auth = oauth)
+
+# Start to streaming
+
+## Set filtering key words
+## Limit tweets language equals to English
+search = "#FRAvPOR"
+iterator = twitter_stream.statuses.filter(track = search, language = "en")
+
+## Set the number of tweets
+tweet_count = 50000
+
+# Streaming tweets
+for tweet in iterator:
+    tweet_count -= 1
+
+    ## Python Twitter Tools will wrap-up 50000 tweets with #GameofThrones
+	## to the JSON format and show them on the screen
+    print json.dumps(tweet)
+
+    if tweet_count <= 0:
+        break
